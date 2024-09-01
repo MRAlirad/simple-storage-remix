8df19ce1d19fe4118bae562e5014f5aeb74afa58
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2024-09-01 17:15:45 +0330

    first commit

---
+++ SimpleStorage.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+//? Version of the solidity
+pragma solidity ^0.8.8; // 0.8.7 and newer version
+
+// to create a contract we use the keyword "contract". it is like "class" in JS
+// "SimpleStorage" => the name of the contract
+contract SimpleStorage {
+    constructor() {
+        
+    }
+}
