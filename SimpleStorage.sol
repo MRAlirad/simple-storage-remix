f00b64dfe015c95e4859b007f70c5d4086aa00d6
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2024-11-13 00:33:46 +0330

    clean up

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: MIT
-//? Version of the solidity
 pragma solidity ^0.8.8; // 0.8.7 and newer version
 
-// to create a contract we use the keyword "contract". it is like "class" in JS
-// "SimpleStorage" => the name of the contract
 contract SimpleStorage {
     uint favouriteNumber = 10;
 
